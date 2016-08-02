72a69f36f7327595ab913e417e8148be4c1eb999
Hiro Asari <asari.ruby@gmail.com>
2016-08-02 15:41:09 -0400

    Implement POST /decode?token=****

--- lib/jwt-decoder-example.rb
+++ lib/jwt-decoder-example.rb
@@ -10,5 +10,18 @@ module JwtDecoder
     get '/' do
       "Hello world!"
     end
+
+    post '/decode' do
+      hmac_secret = 'super_dooper_$ecrets'
+      payload = params['token']
+      logger.debug "token=#{payload}"
+
+      decoded_data = JWT.decode payload, hmac_secret, true, { :algorithm => 'HS256' }
+      logger.debug = "decoded_data=#{decoded_data}"
+
+      "success!"
+    rescue
+      "failure!"
+    end
   end
 end
