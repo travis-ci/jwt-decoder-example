2faae617f1b5f5d6d17858faf9877146f2a62112
Hiro Asari <asari.ruby@gmail.com>
2016-08-02 15:44:23 -0400

    Fix Ruby syntax

--- lib/jwt-decoder-example.rb
+++ lib/jwt-decoder-example.rb
@@ -12,16 +12,18 @@ module JwtDecoder
     end
 
     post '/decode' do
-      hmac_secret = 'super_dooper_$ecrets'
-      payload = params['token']
-      logger.debug "token=#{payload}"
+      begin
+        hmac_secret = 'super_dooper_$ecrets'
+        payload = params['token']
+        logger.debug "token=#{payload}"
 
-      decoded_data = JWT.decode payload, hmac_secret, true, { :algorithm => 'HS256' }
-      logger.debug = "decoded_data=#{decoded_data}"
+        decoded_data = JWT.decode payload, hmac_secret, true, { :algorithm => 'HS256' }
+        logger.debug = "decoded_data=#{decoded_data}"
 
-      "success!"
-    rescue
-      "failure!"
+        "success!"
+      rescue
+        "failure!"
+      end
     end
   end
 end
